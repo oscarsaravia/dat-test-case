f0c463d2cb09ef623272999f7fcc1141034af5e1
oscarsaravia <oscarsaraviard0013@gmail.com>
2023-08-21 21:48:00 -0600

    feat: added car svg and started implementing buttons view

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 
 export const App = () => {
   return (
-    <div>
+    <div className='container'>
       <h1>Application</h1>
       <p>
         This is a React application that is rendered inside a Dataverse
         dashboard.
       </p>
+      <div className="image-container">
+        <img className='car' src="src/assets/car.svg" alt="Car Top View" />
+        <div className="circle"></div>
+      </div>
     </div>
   )
 }
